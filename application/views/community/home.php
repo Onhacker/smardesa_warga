44cd55a6e406e389fbecb2daab5a1070788b1dd5
Onhacker <onhacker@users.noreply.github.com>
2026-09-05 20:05:19 +0800

    feat: ganti gambar slide layanan surat

--- application/views/community/home.php
+++ application/views/community/home.php
@@ -29,7 +29,7 @@
             <div class="splide__track">
                 <div class="splide__list">
             <a class="community-v22-slide splide__slide is-letter" href="<?= site_url('surat') ?>">
-                <img src="<?= base_url('assets/v22/images/pictures/19.jpg') ?>" alt="" loading="lazy" width="700" height="466">
+                <img src="<?= base_url('assets/v22/images/pictures/surat-layanan.webp') ?>" alt="Ilustrasi layanan surat" loading="lazy" width="1537" height="1023">
                 <span class="community-v22-slide-overlay" aria-hidden="true"></span>
                 <span class="community-v22-slide-icon"><i class="fa fa-envelope" aria-hidden="true"></i></span>
                 <span class="community-v22-slide-copy">
