e737b2c3a27523cd154305934ea6ba354778f17a
Onhacker <onhacker@users.noreply.github.com>
2026-09-06 09:06:41 +0800

    style: pusatkan ikon filter pasar

--- application/views/layouts/app.php
+++ application/views/layouts/app.php
@@ -24,7 +24,7 @@ $navSection = $this->uri->segment(1) ?: 'dashboard';
     <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/simp-v22.min.css') ?>?v=1">
     <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/warga.min.css') ?>?v=78">
     <link rel="stylesheet" href="<?= base_url('assets/css/community.min.css') ?>?v=17">
-    <link rel="stylesheet" href="<?= base_url('assets/css/market.css') ?>?v=10">
+    <link rel="stylesheet" href="<?= base_url('assets/css/market.css') ?>?v=11">
     <style id="warga-letters-icon-override">
         body #page .page-content .warga-letters-head .warga-intro-icon,
         body #page .page-content .warga-letters-head .warga-intro-icon > i {
