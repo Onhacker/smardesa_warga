b1af08a759cc0623ad0f04991c7d8a6d4d611d16
Onhacker <onhacker@users.noreply.github.com>
2026-09-08 14:24:05 +0800

    fix: bedakan card daftar pengumuman

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const SDW_CACHE_PREFIX = 'smartdesa-warga-static-';
-const SDW_CACHE = SDW_CACHE_PREFIX + '2026-09-08-notification-74';
+const SDW_CACHE = SDW_CACHE_PREFIX + '2026-09-08-announcement-75';
 const scopeUrl = new URL(self.registration.scope);
 const appPath = scopeUrl.pathname.endsWith('/') ? scopeUrl.pathname : scopeUrl.pathname + '/';
 const offlineUrl = new URL('offline.html', scopeUrl).href;
@@ -23,7 +23,7 @@ const precache = [
   'assets/v22/scripts/custom.min.js?v=3',
   'assets/js/warga.min.js?v=19',
   'assets/js/community.min.js?v=15',
-  'assets/css/community.min.css?v=21',
+  'assets/css/community.min.css?v=22',
   'assets/css/market.css?v=19',
   'assets/js/market.js?v=9',
   'assets/images/market-product-placeholder.svg',
