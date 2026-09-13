a6e9c6ca035ee50335daf0c3492f99630daf6ba0
Onhacker <onhacker@users.noreply.github.com>
2026-09-13 12:08:42 +0900

    Reorganize sidebar navigation

--- application/views/layouts/menu.php
+++ application/views/layouts/menu.php
@@ -17,12 +17,22 @@ $menuRegency = trim((string) ($menuIsAuthenticated
     : ($menuFooterVillage['regency_name'] ?? '')));
 if ($menuRegency === '') $menuRegency = trim((string) (getenv('PUBLIC_REGENCY_NAME') ?: ''));
 if ($menuRegency === '') $menuRegency = trim((string) (getenv('PUBLIC_AREA_NAME') ?: 'Jayawijaya')) ?: 'Jayawijaya';
+$menuMethod = strtolower((string) $this->router->fetch_method());
+$menuLettersActive = nav_is(array('layanan', 'permohonan')) || (nav_is('dashboard') && $menuMethod === 'letters');
+$menuAnnouncementsActive = nav_is('community') && in_array($menuMethod, array('announcements', 'announcement'), TRUE);
+$menuComplaintsActive = nav_is('community') && in_array($menuMethod, array('complaints', 'complaint'), TRUE);
+$menuContactActive = nav_is('community') && $menuMethod === 'contact';
+$menuTokokuActive = nav_is('marketplace') && in_array($menuMethod, array('tokoku', 'store_settings', 'create', 'edit'), TRUE);
+$menuMarketActive = nav_is('marketplace') && !$menuTokokuActive;
+$menuAccountActive = nav_is('account');
+$menuNotificationsActive = nav_is('notifications');
 ?>
 <div class="warga-menu-head">
     <div class="warga-menu-brand-row">
         <img src="<?= warga_asset_url('assets/pwa/icon-192.png') ?>" alt="Logo SI DAPULIK" width="56" height="56">
         <div class="warga-menu-brand-copy">
             <h2>SI DAPULIK</h2>
+            <p class="warga-menu-tagline">Bersama Membangun Kampung Digital</p>
             <div class="warga-menu-location" aria-label="Wilayah layanan">
                 <strong><?= e($menuArea) ?></strong>
                 <?php if ($menuDistrict !== '' || $menuRegency !== ''): ?><span><?= $menuDistrict !== '' ? e($menuDistrict) : '' ?><?= $menuDistrict !== '' && $menuRegency !== '' ? ' · ' : '' ?><?= $menuRegency !== '' ? e($menuRegency) : '' ?></span><?php endif; ?>
@@ -32,33 +42,31 @@ if ($menuRegency === '') $menuRegency = trim((string) (getenv('PUBLIC_AREA_NAME'
     </div>
 </div>
 
-<h6 class="menu-divider mt-4"><?= $staffMode ? 'Pelayanan ' . e($institutionLabel) : 'Layanan' ?></h6>
+<?php if ($staffMode): ?>
+<h6 class="menu-divider mt-4">Pelayanan <?= e($institutionLabel) ?></h6>
 <div class="list-group list-custom-small list-menu warga-menu-list">
-    <?php if ($staffMode): ?>
-        <a class="<?= nav_is('petugas') ? 'active-nav' : '' ?>" href="<?= site_url('petugas') ?>"><i class="fa fa-chart-pie warga-menu-icon is-green"></i><span>Ringkasan Layanan</span><i class="fa fa-angle-right"></i></a>
-        <a href="<?= site_url('petugas?status=submitted') ?>"><i class="fa fa-clipboard-check warga-menu-icon is-blue"></i><span>Antrean Verifikasi</span><i class="fa fa-angle-right"></i></a>
-        <a href="<?= site_url('petugas?status=verified') ?>"><i class="fa fa-user-check warga-menu-icon is-amber"></i><span>Menunggu Persetujuan</span><i class="fa fa-angle-right"></i></a>
-        <a href="<?= site_url('petugas?status=issued') ?>"><i class="fa fa-file-pdf warga-menu-icon is-purple"></i><span>Surat Terbit</span><i class="fa fa-angle-right"></i></a>
-    <?php else: ?>
-        <a class="<?= nav_is('dashboard') ? 'active-nav' : '' ?>" href="<?= site_url('dashboard') ?>"><i class="fa fa-home warga-menu-icon is-green"></i><span>Beranda</span><i class="fa fa-angle-right"></i></a>
-        <a class="<?= nav_is('layanan') ? 'active-nav' : '' ?>" href="<?= site_url('layanan') ?>"><i class="fa fa-envelope warga-menu-icon is-teal"></i><span>Surat</span><i class="fa fa-angle-right"></i></a>
-        <a class="<?= nav_is('permohonan') && $this->router->fetch_method() === 'create' ? 'active-nav' : '' ?>" href="<?= site_url('layanan') ?>"><i class="fa fa-plus warga-menu-icon is-blue"></i><span>Permohonan Baru</span><i class="fa fa-angle-right"></i></a>
-        <a class="<?= nav_is('permohonan') && $this->router->fetch_method() !== 'create' ? 'active-nav' : '' ?>" href="<?= site_url('permohonan') ?>"><i class="fa fa-file-alt warga-menu-icon is-sand"></i><span>Riwayat Permohonan</span><i class="fa fa-angle-right"></i></a>
-        <?php if ($menuIsAuthenticated): ?><a class="<?= nav_is('notifications') ? 'active-nav' : '' ?>" href="<?= site_url('notifikasi') ?>"><i class="fa fa-inbox warga-menu-icon is-red"></i><span>Pemberitahuan</span><i class="fa fa-angle-right"></i></a><?php endif; ?>
-    <?php endif; ?>
+    <a class="<?= nav_is('petugas') ? 'active-nav' : '' ?>" href="<?= site_url('petugas') ?>"><i class="fa fa-chart-pie warga-menu-icon is-green"></i><span>Ringkasan Layanan</span><i class="fa fa-angle-right"></i></a>
+    <a href="<?= site_url('petugas?status=submitted') ?>"><i class="fa fa-clipboard-check warga-menu-icon is-blue"></i><span>Antrean Verifikasi</span><i class="fa fa-angle-right"></i></a>
+    <a href="<?= site_url('petugas?status=verified') ?>"><i class="fa fa-user-check warga-menu-icon is-amber"></i><span>Menunggu Persetujuan</span><i class="fa fa-angle-right"></i></a>
+    <a href="<?= site_url('petugas?status=issued') ?>"><i class="fa fa-file-pdf warga-menu-icon is-purple"></i><span>Surat Terbit</span><i class="fa fa-angle-right"></i></a>
+</div>
+<?php endif; ?>
+
+<h6 class="menu-divider mt-4">Layanan</h6>
+<div class="list-group list-custom-small list-menu warga-menu-list">
+    <a class="<?= $menuMarketActive ? 'active-nav' : '' ?>" href="<?= site_url('pasar') ?>"><i class="fa fa-store warga-menu-icon is-green"></i><span>Pasar Dapulik</span><i class="fa fa-angle-right"></i></a>
+    <a class="<?= $menuLettersActive ? 'active-nav' : '' ?>" href="<?= site_url('surat') ?>"><i class="fa fa-envelope warga-menu-icon is-teal"></i><span>Surat</span><i class="fa fa-angle-right"></i></a>
+    <a class="<?= $menuAnnouncementsActive ? 'active-nav' : '' ?>" href="<?= site_url('pengumuman') ?>"><i class="fa fa-bullhorn warga-menu-icon is-purple"></i><span>Pengumuman</span><i class="fa fa-angle-right"></i></a>
+    <a class="<?= $menuComplaintsActive ? 'active-nav' : '' ?>" href="<?= site_url('pengaduan') ?>"><i class="fa fa-comments warga-menu-icon is-amber"></i><span>Aduan</span><i class="fa fa-angle-right"></i></a>
 </div>
 
 <h6 class="menu-divider mt-4">Akun</h6>
 <div class="list-group list-custom-small list-menu warga-menu-list">
-    <a href="<?= site_url('pengumuman') ?>"><i class="fa fa-bullhorn warga-menu-icon is-purple"></i><span>Pengumuman</span><i class="fa fa-angle-right"></i></a>
-    <a href="<?= site_url('pengaduan') ?>"><i class="fa fa-comments warga-menu-icon is-amber"></i><span>Pengaduan</span><i class="fa fa-angle-right"></i></a>
-    <a class="<?= nav_is('marketplace') ? 'active-nav' : '' ?>" href="<?= site_url('pasar') ?>"><i class="fa fa-store warga-menu-icon is-green"></i><span>Pasar Dapulik</span><i class="fa fa-angle-right"></i></a>
-    <?php if ($menuIsAuthenticated && $menuCanManageMarketplace): ?><a href="<?= site_url('pasar/tokoku') ?>"><i class="fa fa-store-alt warga-menu-icon is-blue"></i><span>Tokoku</span><i class="fa fa-angle-right"></i></a><?php endif; ?>
-    <a href="<?= site_url('kontak') ?>"><i class="fa fa-address-book warga-menu-icon is-teal"></i><span>Kontak <?= e($institutionLabel) ?></span><i class="fa fa-angle-right"></i></a>
-    <?php if ($staffMode): ?><a href="<?= site_url('notifikasi') ?>"><i class="fa fa-inbox warga-menu-icon is-red"></i><span>Pemberitahuan</span><i class="fa fa-angle-right"></i></a><?php endif; ?>
-    <?php if ($menuIsAuthenticated): ?><a class="<?= nav_is('account') ? 'active-nav' : '' ?>" href="<?= site_url('akun') ?>"><i class="fa fa-user warga-menu-icon is-indigo"></i><span>Akun Saya</span><i class="fa fa-angle-right"></i></a><?php else: ?><a href="<?= site_url('login') ?>"><i class="fa fa-sign-in-alt warga-menu-icon is-blue"></i><span>Login</span><i class="fa fa-angle-right"></i></a><?php endif; ?>
+    <?php if ($menuIsAuthenticated && $menuCanManageMarketplace): ?><a class="<?= $menuTokokuActive ? 'active-nav' : '' ?>" href="<?= site_url('pasar/tokoku') ?>"><i class="fa fa-store-alt warga-menu-icon is-blue"></i><span>Tokoku</span><i class="fa fa-angle-right"></i></a><?php endif; ?>
+    <?php if ($menuIsAuthenticated): ?><a class="<?= $menuAccountActive ? 'active-nav' : '' ?>" href="<?= site_url('akun') ?>"><i class="fa fa-user warga-menu-icon is-indigo"></i><span>Akun Saya</span><i class="fa fa-angle-right"></i></a><?php else: ?><a href="<?= site_url('login') ?>"><i class="fa fa-sign-in-alt warga-menu-icon is-blue"></i><span>Login</span><i class="fa fa-angle-right"></i></a><?php endif; ?>
+    <a class="<?= $menuContactActive ? 'active-nav' : '' ?>" href="<?= site_url('kontak') ?>"><i class="fa fa-address-book warga-menu-icon is-teal"></i><span>Kontak Kampung</span><i class="fa fa-angle-right"></i></a>
     <a href="#" data-toggle-theme class="warga-menu-toggle-row"><i class="fa fa-moon warga-menu-icon is-dark"></i><span>Mode Gelap</span><div class="custom-control small-switch ios-switch warga-menu-toggle-control"><input data-toggle-theme type="checkbox" class="ios-input" id="switch-dark-mode"><label class="custom-control-label" for="switch-dark-mode"></label></div></a>
-    <?php if ($menuIsAuthenticated): ?><label class="warga-menu-toggle-row" data-push-toggle-row><i class="fa fa-inbox warga-menu-icon is-red"></i><span>Pemberitahuan</span><span class="custom-control small-switch ios-switch warga-menu-toggle-control"><input data-push-toggle type="checkbox" class="ios-input" id="switch-menu-push-notification" aria-label="Nyalakan pemberitahuan"><span class="custom-control-label" aria-hidden="true"></span></span></label><?php endif; ?>
+    <?php if ($menuIsAuthenticated): ?><label class="warga-menu-toggle-row <?= $menuNotificationsActive ? 'active-nav' : '' ?>" data-push-toggle-row><i class="fa fa-inbox warga-menu-icon is-red"></i><span>Pemberitahuan</span><span class="custom-control small-switch ios-switch warga-menu-toggle-control"><input data-push-toggle type="checkbox" class="ios-input" id="switch-menu-push-notification" aria-label="Nyalakan pemberitahuan"><span class="custom-control-label" aria-hidden="true"></span></span></label><?php endif; ?>
     <?php if ($menuIsAuthenticated): ?><form method="post" action="<?= site_url('logout') ?>" class="warga-logout-form" data-logout-form>
         <?= csrf_field() ?>
         <button type="submit"><i class="fa fa-sign-out-alt warga-menu-icon is-red"></i><span>Keluar</span><i class="fa fa-angle-right"></i></button>
